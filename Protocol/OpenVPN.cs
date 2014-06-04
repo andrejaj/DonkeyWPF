17451a0cc02b69acfb4b98b8b48d3ccf158bab9b
andreja <andreja.jovanovic@dwellnt.com>
2014-06-04 14:43:11 +0100

    added missed folder.

---
+++ Protocol/OpenVPN.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Infrastructure;
+
+namespace Protocol
+{
+    public class OpenVPN : IVpnProtocol
+    {
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+
+        public OpenVPN(string ipAddress, int port) 
+        {
+            IpAddress = ipAddress;
+            Port = port;
+        }
+
+        public OpenVPN() { }
+        public void Connect() 
+        {
+            Thread.Sleep(500);
+        }
+
+        public void Disconnect()
+        {
+            Thread.Sleep(700);
+        }
+    }
+}
---
+++ Protocol/PPTP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Infrastructure;
+
+namespace Protocol
+{
+    public class PPTP : IVpnProtocol
+    {
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+        public PPTP(string ipAddress, int port) 
+        {
+            IpAddress = ipAddress;
+            Port = port;
+        }
+        public PPTP() { }
+        public void Connect() 
+        {
+            Thread.Sleep(900);
+        }
+
+        public void Disconnect()
+        {
+            Thread.Sleep(500);
+        }
+    }
+}
